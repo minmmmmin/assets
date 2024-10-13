faee6be3e4c336115540e8dbb6a1c4dad956a012
minmmmmin <kotomi.hinahima@gmail.com>
2024-10-13 14:48:35 +0900

    imageパスの変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <h1>BIRDWATCHING</h1>
-            <img src="public\images\dove.png" alt="a simple dove logo" />
+            <img src="\images\dove.png" alt="a simple dove logo" />
             <Navi />
         </header>
     );
